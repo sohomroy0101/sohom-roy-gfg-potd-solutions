5b0980f9beadae66a2c63360847c9a58a89dd61d
Sohom Roy <rsohom426@gmail.com>
2026-07-15 21:44:31 +0530

    gfg potd solution 15th July 2026 added

---
+++ Longest Bitonic Subarray.cpp
@@ -0,0 +1,23 @@
+//Problem: Longest Bitonic Subarray
+//geeksforgeeks problem of the day -> 15th July 2026
+// C++ CODE
+class Solution {
+public:
+    int bitonic(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> inc(n, 1), dec(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] >= arr[i - 1])
+                inc[i] = inc[i - 1] + 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= arr[i + 1])   
+                dec[i] = dec[i + 1] + 1;
+        }
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, inc[i] + dec[i] - 1);
+        }
+        return ans;
+    }
+};
